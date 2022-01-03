73e00ea5d711e9f3a81c04135cfaff718b4f779f
gonnagetbetter <denissss906@gmail.com>
2022-01-03 21:48:18 +0200

    Adding pipe

---
+++ Ex6/pipe.js
@@ -0,0 +1,33 @@
+const pipe = (...fns) => {
+    for(const func of fns){
+        if(typeof func !== 'function'){
+            throw new Error('Not function');
+        }
+    }
+    return x => fns.reduce((g,f) => f(g),x);
+}
+
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+{
+
+const f = pipe(inc, twice, cube);
+console.log(f(5));
+
+}
+
+{
+
+const f = pipe(inc, inc);
+console.log(f(7));
+
+}
+
+{
+
+const f = pipe(inc, 7, cube);
+console.log(f);
+
+}
