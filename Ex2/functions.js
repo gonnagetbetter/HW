32e61a8b4892d5a311c391430445a787412e3aa9
gonnagetbetter <denissss906@gmail.com>
2022-01-05 21:00:16 +0200

    Corrected with Eslint

--- Ex2/functions.js
+++ Ex2/functions.js
@@ -1,32 +1,32 @@
-'use strict'
- 
-const Array = [];
-let sqr, 
-    cub,
-    avg;
+'use strict';
 
-const average = (a, b) =>{
-    avg = ( a + b) / 2 ;
-    return avg;
+const Array = [];
+let sqr;
+let cub;
+let avg;
+
+const average = (a, b) => {
+  avg = ( a + b) / 2;
+  return avg;
 };
 
 const square = (a) => {
-    sqr = a * a;
-    return sqr;
+  sqr = a * a;
+  return sqr;
 };
 
 const cube = (a) =>{
-     cub = a * a * a; 
-     return cub;
+  cub = a * a * a;
+  return cub;
 };
 
 const calculate = (Array) => {
-    Array.forEach( number => console.log(number));
+  Array.forEach( (number) => console.log(number));
 };
 
- for (let i = 0; i < 9; i++){
-    average(square(i),cube(i));
-    Array.push(avg);
- };
+for (let i = 0; i < 9; i++) {
+  average(square(i), cube(i));
+  Array.push(avg);
+};
 
 calculate(Array);
