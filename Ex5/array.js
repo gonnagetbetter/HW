06adf688c45bc2c9433dafbd5b3faab2fb7f9645
gonnagetbetter <denissss906@gmail.com>
2022-01-05 22:06:48 +0200

    Corrected with Eslint

--- Ex5/array.js
+++ Ex5/array.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const array = () => {
-    const data = [];
-    const meth = (i) => data[i];
-    meth.push = (val) => data.push(val);
-    meth.pop = () => data.pop();
-    return meth;
-};  
+  const data = [];
+  const meth = (i) => data[i];
+  meth.push = (val) => data.push(val);
+  meth.pop = () => data.pop();
+  return meth;
+};
 
 const arr = array();
 
@@ -14,12 +14,12 @@ arr.push('first');
 arr.push('second');
 arr.push('third');
 
-console.log(arr(0)); 
-console.log(arr(1)); 
+console.log(arr(0));
+console.log(arr(1));
 console.log(arr(2));
 
-console.log(arr.pop()); 
-console.log(arr.pop()); 
-console.log(arr.pop()); 
+console.log(arr.pop());
+console.log(arr.pop());
+console.log(arr.pop());
 
 console.log(arr.pop());
