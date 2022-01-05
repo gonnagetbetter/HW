e5ee0f439a0f8cab477f3583d802d25a003bbcdb
gonnagetbetter <denissss906@gmail.com>
2022-01-05 21:58:46 +0200

    Corrected with Eslint

--- Ex4/matrix.js
+++ Ex4/matrix.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const Max = (Array) => {
-    let maxV = Array[0][0];
-    for(let i = 0; i < Array.length; i++){
-        for(let j = 0; j < Array[i].length; j++){
-            let value = Array[i][j];
-            if(maxV < value) maxV = value;
-        };
+const max = (Array) => {
+  let maxV = Array[0][0];
+  for (let i = 0; i < Array.length; i++) {
+    for (let j = 0; j < Array[i].length; j++) {
+      let value = Array[i][j];
+      if (maxV < value) maxV = value;
     };
-    return maxV;
+  };
+  return maxV;
 };
-  
-console.log(Max([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+
+console.log(max([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
