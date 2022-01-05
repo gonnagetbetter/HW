180e8876a7da4aba93be35023cc17e21e64d4425
gonnagetbetter <denissss906@gmail.com>
2022-01-05 22:13:39 +0200

    Corrected with Eslint

--- Ex6/pipe.js
+++ Ex6/pipe.js
@@ -1,36 +1,29 @@
 'use strict';
 
 const pipe = (...fns) => {
-    for(const funct of fns){
-        if(typeof funct !== 'function'){
-            throw new Error('Not function');
-        }
+  for (const funct of fns) {
+    if (typeof funct !== 'function') {
+      throw new Error('Not function');
     }
-    return x=> fns.reduce((g,f)=>f(g),x);
+  }
+  return (x) => fns.reduce((g, f)=>f(g), x);
+};
 
-}
-
-const inc = x => ++x;
-const twice = x => x * 2;
-const cube = x => x ** 3;
-
-{
-
-const f = pipe(inc, twice, cube);
-console.log(f(5));
-
-}
+const inc = (x) => ++x;
+const twice = (x) => x * 2;
+const cube = (x) => x ** 3;
 
 {
-
-const f = pipe(inc, inc);
-console.log(f(7));
-
+  const f = pipe(inc, twice, cube);
+  console.log(f(5));
 }
 
 {
+  const f = pipe(inc, inc);
+  console.log(f(7));
+}
 
-const f = pipe(inc, 7, cube);
-console.log('Unreachable');
-
+{
+  const f = pipe(inc, 7, cube);
+  console.log('Unreachable');
 }
