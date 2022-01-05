3e321b1ba94f24ff3f0b07b47e6cd9e7428b761a
gonnagetbetter <denissss906@gmail.com>
2022-01-05 22:09:01 +0200

    Corrected with Eslint

--- Ex5/Closed.js
+++ Ex5/Closed.js
@@ -2,12 +2,11 @@
 
 const seq = (f) => (g) => (typeof g === 'number' ? f(g) : seq((x) => f(g(x))));
 
-let a = seq(x => x + 7)(x => x * 2)(5);
+const a = seq( (x) => x + 7)( (x) => x * 2)(5);
 console.log(a);
 
-let b = seq(x => x * 2)(x => x + 7)(5);
+const b = seq( (x) => x * 2)( (x) => x + 7)(5);
 console.log(b);
 
-let c = seq(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7);
+const c = seq( (x) => x + 1)( (x) => x * 2)( (x) => x / 3)( (x) => x - 4)(7);
 console.log(c);
-
