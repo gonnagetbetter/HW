c80c34846852e2defcb8150e2b36206172877206
gonnagetbetter <denissss906@gmail.com>
2022-01-05 22:01:55 +0200

    Corrected with Eslint

--- Ex4/objectIteration.js
+++ Ex4/objectIteration.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const persons = {
-    lenin: { born: 1870, died: 1924 },
-    mao: { born: 1893, died: 1976 },
-    gandhi: { born: 1869, died: 1948 },
-    hirohito: { born: 1901, died: 1989 },
+  lenin: {born: 1870, died: 1924},
+  mao: {born: 1893, died: 1976},
+  gandhi: {born: 1869, died: 1948},
+  hirohito: {born: 1901, died: 1989},
 };
 
 const ages = (persons) =>{
-    let Ages = {};
-    for (const name in persons){
-        let person = persons[name];
-            Ages[name] = person.died - person.born;
-    }
-    return Ages;
+  const Ages = {};
+  for (const name in persons) {
+    let person = persons[name];
+      Ages[name] = person.died - person.born;
+  }
+  return Ages;
 };
 
 console.log(ages(persons));
