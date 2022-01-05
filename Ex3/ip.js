93c81768936240d002c1425b5e8e7cf16c3c9bea
gonnagetbetter <denissss906@gmail.com>
2022-01-05 21:50:11 +0200

    Corrected with Eslint

--- Ex3/ip.js
+++ Ex3/ip.js
@@ -2,32 +2,34 @@
 
 const Ip = '10.0.0.1';
 
-const StrToArr = (str) => {
-    let Arr = str.split('.');
-    Arr = Arr.map(Number);
-    return Arr
-}
-    
-const Shift = (Arr) =>{
-    const lenth = Arr.length - 1;
-    for (let i = 0; i < lenth; i++){
-        for (let n = lenth - 1;  n >= 0; n--){
-            Arr[n] = Arr[n] << 8; 
-        }
+const strToArr = (str) => {
+  let Arr = str.split('.');
+  Arr = Arr.map(Number);
+  return Arr;
+};
+
+const shift = (Arr) =>{
+  const lenth = Arr.length - 1;
+  for (let i = 0; i < lenth; i++) {
+    for (let n = lenth - 1;  n >= 0; n--) {
+      Arr[n] = Arr[n] << 8;
     }
-    return Arr 
-}
+  }
+  return Arr;
+};
 
 const add = (Arr) => {
-    let sum = 0;
-    Arr.forEach(element => {
-       sum += element;
-    }); 
-    return sum;
-}
+  let sum = 0;
+  Arr.forEach( (element) => {
+    sum += element;
+  });
+  return sum;
+};
 
-const BetterAdder = (previousValue, currentValue) => previousValue + currentValue;
+const betterAdder = (previousValue, currentValue) => {
+  previousValue + currentValue;
+};
 
-console.log((Shift(StrToArr(Ip)).reduce(BetterAdder)));
+console.log((shift(strToArr(Ip)).reduce(betterAdder)));
 
-console.log(add(Shift(StrToArr(Ip))));
+console.log(add(shift(strToArr(Ip))));
